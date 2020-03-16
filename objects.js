150c0b74f370ce9f4e8cce4f4f0432d3aea4f289
Jackson Chen <jacksonchen171@gmail.com>
2020-03-16 01:07:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function updatePlaylist(playlist, artist, song){
   playlist[artist] = song
   return playlist
 }
+
+function removeFromPlayList(playlist, artist){
+  delete playlist[artist]
+  return playlist
+}
